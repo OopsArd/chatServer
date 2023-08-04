c3f63848f5958ccf0578ae8c685bd64aae4f6246
Dung <Dung@LAPTOP-PS0C7HOA>
2023-08-04 14:16:45 +0700

    update socket

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,76 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    private ServerSocket serverSocket;
+    private List<ClientHandler> clients = new ArrayList<>();
 
-        System.out.printf("Hello and welcome!");
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        System.out.println("Server started on port " + port);
 
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected: " + socket);
+                ClientHandler clientHandler = new ClientHandler(socket);
+                clients.add(clientHandler);
+                new Thread(clientHandler).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public static void main(String[] args) throws IOException {
+        int port = 12345; // Choose any available port
+        Main chatServer = new Main();
+        chatServer.start(port);
+    }
+
+    private class ClientHandler implements Runnable {
+        private Socket clientSocket;
+        private PrintWriter out;
+        private BufferedReader in;
+
+        public ClientHandler(Socket socket) {
+            this.clientSocket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Received from client: " + inputLine);
+                    broadcastMessage(inputLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                    clients.remove(this);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+    }
+
+    private void broadcastMessage(String message) {
+        for (ClientHandler client : clients) {
+            client.sendMessage(message);
+        }
     }
 }
